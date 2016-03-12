05dd6a8467959fddecb88fdeff940c8308a52f30
Philipp Liegl <philipp.liegl@ecosio.com>
2016-03-12 13:19:00 +0100

    CHANGE moved data store logic to a dedicated class

---
+++ server/datastore.js
@@ -0,0 +1,184 @@
+'use strict';
+/**
+Data store abstracting from the bids.json file
+*/
+class Datastore {
+
+  /**
+  *Creates a new Datastore
+  */
+  constructor(FILEPATH, fs, uuid) {
+    this.FILEPATH = FILEPATH;
+    this.fs = fs;
+    this.uuid = uuid;
+  }
+
+  //Return all bids
+  //The operation is async - as soon as the read operation is finished, the
+  //callback is invoked
+  getBids(callback) {
+    this.fs.readFile(this.FILEPATH, callback);
+  }
+
+  //Write the bid to the file
+  //The operation is async - as soon as the write operation is finished, the
+  //callback is invoked
+  writeBid(newItem, callback) {
+    let _this = this;
+    _this.fs.readFile(_this.FILEPATH, function (err, data) {
+      if (err) {
+        console.error('Unable to get bid items from file ' + err);
+        callback(err, null);
+        return;
+      }
+
+      //Get the items from the file
+      let bidItems;
+      //Consider empty files
+      if (data == undefined || data.length==0) {
+        bidItems = [];
+      }
+      else {
+        bidItems = JSON.parse(data);
+      }
+
+
+      //Store the new item
+      bidItems = _this.slice(bidItems);
+      //Why unshift is not called 'insert' will always remain a mystery...
+      bidItems.unshift(newItem);
+
+      //Write the items back to the file and return the updated store using then
+      //callback
+      _this.fs.writeFile(_this.FILEPATH, JSON.stringify(bidItems, null, 4), function (err) {
+        if (err) {
+          console.error('Unable to write items to file ' + err);
+        }
+
+        callback(err, bidItems);
+      });
+    });
+  }
+
+  //Search for the existing bid and update it
+  //The operation is async - as soon as the update operation is finished, the
+  //callback is invoked
+  updateBid(newItem, callback) {
+
+    let _this = this;
+
+    _this.fs.readFile(_this.FILEPATH, function (err, data) {
+      if (err) {
+        console.error('Unable to get bid items from file ' + err);
+        callback(err, null);
+        return;
+      }
+
+      //Get the items from the file
+      let bidItems;
+      //Consider empty files
+      if (data == undefined || data.length==0) {
+        bidItems = [];
+      }
+      else {
+        bidItems = JSON.parse(data);
+      }
+
+      //Search for the bid item based on the ID and replace it, in case it
+      //is found
+      let found = false;
+      for (let i = 0; i < bidItems.length; i++) {
+        if (bidItems[i].id === newItem.id) {
+          bidItems[i] = newItem;
+          found = true;
+          console.log('Found an entry for the given bid item. Replacing it.');
+          break;
+        }
+      }
+
+      //In case we were unable to find the respective bid item, we simply
+      //store it as new new one
+      if (!found) {
+        console.log('Found no entry for the given bid item. Storing a new entry.');
+
+        //Reset the id to a UUID in order to have consistent IDs
+        newItem.id = _this.uuid.v4();
+        console.log('(Re)setting its id to ' + newItem.id);
+        bidItems = _this.slice(bidItems);
+        bidItems.unshift(newItem);
+      }
+
+      //Write the items back to the file and return the updated store using then
+      //callback
+      _this.fs.writeFile(_this.FILEPATH, JSON.stringify(bidItems, null, 4), function (err) {
+        if (err) {
+          console.error('Unable to write items to file ' + err);
+        }
+
+        callback(err, bidItems);
+      });
+    });
+  }
+
+  //Delete the bid with the given id
+  //The operation is async - as soon as the delete operation is finished, the
+  //callback is invoked
+  deleteBid(bidId, callback) {
+
+    let _this = this;
+
+    _this.fs.readFile(_this.FILEPATH, function (err, data) {
+      if (err) {
+        console.error('Unable to get bid items from file ' + err);
+        callback('Unable to access bid items', null);
+        return;
+      }
+
+      //Get the items from the file
+      let bidItems;
+      //Consider empty files
+      if (data == undefined || data.length==0) {
+        bidItems = [];
+      }
+      else {
+        bidItems = JSON.parse(data);
+      }
+      console.log('Received a DELETE with the following id ' + bidId);
+
+      //Simply filter out the item to be deleted
+      let updatedbidItems = bidItems.filter((entry) => {return entry.id != bidId});
+
+      //Write the filtered items back to the file
+      _this.fs.writeFile(_this.FILEPATH, JSON.stringify(updatedbidItems, null, 4), function (err) {
+        if (err) {
+          console.error('Unable to write items to file ' + err);
+          callback('Unable to access bid items', null);
+          return;
+        }
+
+        if (bidItems.length == updatedbidItems.length) {
+          callback('Could not find bid item with id ' + bidId, null);
+        } else {
+          callback(null, 'Successfully deleted bid item with id ' + bidId);
+        }
+      });
+    });
+  }
+
+  //Slices the bid array and limits its size to 30 entries
+  slice(bids) {
+    if (bids.length > 3) {
+      console.log('Data store size is too large, slicing.');
+      bids = bids.slice(0,3);
+    }
+    return bids;
+  }
+
+
+}
+
+//ES6 modules not supported by node.js by default
+//export default Datastore;
+
+//Go with the CommonJS approach instead
+module.exports = Datastore;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
+'use strict';
 /* A simple server for fetching, adding, deleting, and upating bid items
   Bid items are stored in a bids.json file
   TODO: HEAD and OPTIONS operations should be implemented */
 
-'use strict';
-
 //Use express as web server
 let express = require('express');
 let app = express();
+
+//Get the environment, we are currently operating on
+let ENVIRONMENT = app.get('env');
 
 //Use body parser for JSON processing in request data
 let bodyParser = require('body-parser');
@@ -14,7 +16,11 @@ let fs = require('fs');
 let path = require('path');
 let uuid = require('node-uuid');
 
+//Load the datastore for the bids
 const FILEPATH = path.join(__dirname, 'bids.json');
+let Datastore = require('./datastore.js');
+const ds = new Datastore(FILEPATH, fs, uuid);
+
 const PORT = process.env.PORT || 3000;
 const APIPATH = '/b3a/api/v1/';
 
@@ -24,6 +30,24 @@ app.use(express.static('static'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Register a global error handler, to deal with invalid JSON
+//Prevents node.js internal stack traces to be reported back to the client
+app.use(function (err, req, res, next) {
+  if(err){
+   console.log(err);
+  }
+  next();
+});
+
+//Set a global origin policy to allow cross-domain access - development mode only
+app.use('/', function(req, res, next) {
+  if (ENVIRONMENT == 'development') {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,  Content-Type, Accept");
+  }
+  next();
+});
+
 const ROUTER = express.Router();
 
 //Operations to be applied on the bids collection
@@ -32,21 +56,34 @@ ROUTER.route('/bids')
     //GET /b3a/api/v1/bids
     //Get all stored bid items
     .get(function (req, res) {
-      fs.readFile(FILEPATH, (err, data) => {
-        if (err) {
-          console.error('Unable to get bid items from file ' + err);
-        }
 
-        res.setHeader('Cache-Control', 'no-cache');
-        res.json(JSON.parse(data));
-      });
+      //Callback, invoked after file operation is complete
+      let returnBids = function (err, data) {
+        //Read failed
+        if (err) {
+          error(res, 'Unable to access data store on server.');
+        } else {
+
+          //Consider empty file content and return empty JSON in this case
+          if (data == undefined || data.length==0) {
+            data = ['[]'];
+          }
+
+          res.setHeader('Cache-Control', 'no-cache');
+          res.json(JSON.parse(data));
+        }
+      };
+
+      //Access the file and return the bids
+      ds.getBids(returnBids);
+
     })
 
     //POST /b3a/api/v1/bids
     //Create a new bid item on the server
     .post(function (req, res) {
 
-      //Deal with JSON encoded bodies only
+      //Content-Type must be JSON
       if (!isHeaderSet(req, res)) return;
 
       //Validate, what the user has submitted
@@ -63,29 +100,19 @@ ROUTER.route('/bids')
       newItem.id = uuid.v4();
       console.log('(Re)setting its id to ' + newItem.id);
 
-      //All good - continue writing the received JSON
-      fs.readFile(FILEPATH, function (err, data) {
+      //Callback, invoked after file operation is complete
+      let returnBids = function (err, data) {
+        //Read failed
         if (err) {
-          console.error('Unable to get bid items from file ' + err);
-        }
-
-        //Get the items from the file
-        let bidItems = JSON.parse(data);
-
-        //Store then new item
-        bidItems.push(newItem);
-
-        //Write the items back to the file and return the updated store to the client
-        fs.writeFile(FILEPATH, JSON.stringify(bidItems, null, 4), function (err) {
-          if (err) {
-            console.error('Unable to write items to file ' + err);
-          }
-
+          error(res, 'Unable to access data store on server.');
+        } else {
           res.setHeader('Cache-Control', 'no-cache');
-          res.json(bidItems);
-        });
+          res.json(data);
+        }
+      };
 
-      });
+      //Write the bid to the file
+      ds.writeBid(newItem, returnBids);
     })
 
     //PUT /b3a/api/v1/bids
@@ -105,86 +132,38 @@ ROUTER.route('/bids')
         return;
       }
 
-      fs.readFile(FILEPATH, function (err, data) {
+      //Callback, invoked after file operation is complete
+      let returnBids = function (err, data) {
+        //Read failed
         if (err) {
-          console.error('Unable to get bid items from file ' + err);
-        }
-
-        //Get the items from the file
-        let bidItems = JSON.parse(data);
-
-        //Search for the bid item based on the ID and replace it, in case it
-        //is found
-        let found = false;
-        for (let i = 0; i < bidItems.length; i++) {
-          if (bidItems[i].id === newItem.id) {
-            bidItems[i] = newItem;
-            found = true;
-            console.log('Found an entry for the given bid item. Replacing it.');
-            break;
-          }
-        }
-
-        //In case we were unable to find the respective bid item, we simply
-        //store it as new new one
-        if (!found) {
-          console.log('Found no entry for the given bid item. Storing a new entry.');
-
-          //Reset the id to a UUID in order to have consistent IDs
-          newItem.id = uuid.v4();
-          console.log('(Re)setting its id to ' + newItem.id);
-          bidItems.push(newItem);
-        }
-
-        //Write the items back to the file and return the updated store to the client
-        fs.writeFile(FILEPATH, JSON.stringify(bidItems, null, 4), function (err) {
-          if (err) {
-            console.error('Unable to write items to file ' + err);
-          }
-
+          error(res, 'Unable to access data store on server.');
+        } else {
           res.setHeader('Cache-Control', 'no-cache');
-          res.json(bidItems);
-        });
+          res.json(data);
+        }
+      };
 
-      });
+      ds.updateBid(newItem, returnBids);
+
     });
 
-//Operations, specific of a certain entry
+//Operations, specific to a certain entry
 ROUTER.route('/bids/:bid_id')
 
   //DELETE /b3a/api/v1/bids/:bid_id
   //Delete an existing item on the server
   .delete(function (req, res) {
 
-    fs.readFile(FILEPATH, function (err, data) {
+    //Callback for returning the status of the delete operation as JSON message
+    let notifyClient = function (err, success) {
       if (err) {
-        console.error('Unable to get bid items from file ' + err);
+        error(res, err);
+      } else if (success) {
+        info(res, success);
       }
+    };
 
-      //Get the items from the file
-      let bidItems = JSON.parse(data);
-      console.log('Received a DELETE with the following id ' + req.params.bid_id);
-
-      //Simply filter out the item to be deleted
-      let updatedbidItems = bidItems.filter((entry) => {return entry.id != req.params.bid_id});
-
-
-      //Write the filtered items back to the file
-      fs.writeFile(FILEPATH, JSON.stringify(updatedbidItems, null, 4), function (err) {
-        if (err) {
-          console.error('Unable to write items to file ' + err);
-        }
-
-        //Report a simple message back, wrapped in a JSON object
-        if (bidItems.length == updatedbidItems.length) {
-          error(res, 'Could not find bid item with id' + req.params.bid_id);
-        } else {
-          info(res, 'Successfully deleted bid item with id' + req.params.bid_id);
-        }
-
-      });
-
-    });
+    ds.deleteBid(req.params.bid_id, notifyClient);
 
   });
 
@@ -192,7 +171,7 @@ ROUTER.route('/bids/:bid_id')
 app.use(APIPATH, ROUTER);
 
 app.listen(PORT, function () {
-  console.log('B3A app listening on port ' + PORT + ' Access app under ' + APIPATH);
+  console.log('B3A app listening on port ' + PORT + ' in ' + ENVIRONMENT + ' mode. Access app under ' + APIPATH);
 });
 
 ////////////////
@@ -222,6 +201,10 @@ function isHeaderSet(req, res) {
 
 //Validate the request
 function validateRequest(newItem) {
+
+  if (newItem.length > 1) {
+    return 'You may only submit one entry per request.';
+  }
 
   if (!newItem.name) {
     return 'You must provide a name.';
